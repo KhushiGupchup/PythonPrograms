80e0d60cb36ef25c852a055bf9f1f84e52842f15
KhushiGupchup <khushigupchup@gmail.com>
2025-07-16 12:29:32 +0530

    Create Day1_Welcome_msg_generator.py

---
+++ Day1_Welcome_msg_generator.py
@@ -0,0 +1,14 @@
+#Welcome Message Generator
+
+#step1: Ask For user details
+
+name = input("What is your name ?" )
+hobby = input("What's your favourite hobby")
+
+#step 2: Genaerate a personalized welcome message
+
+print("\n ---Welcome Message---")
+print(f"Hello,{name}")
+print(f"Welcome to the world of programming")
+print(f"It's great to know that you love {hobby} ")
+print(f"Get ready to build something amazing today.")
