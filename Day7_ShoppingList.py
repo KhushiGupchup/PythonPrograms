a8a989cff79396c410bba769431d1452d2b76741
KhushiGupchup <khushigupchup@gmail.com>
2025-07-20 12:54:48 +0530

    Create Day7_ShoppingList.py

---
+++ Day7_ShoppingList.py
@@ -0,0 +1,21 @@
+#Number Comparison Tool
+
+#Step 1: Get user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#Step 2: Compare the numbers
+print("\n--- Comparison Results ---")
+if num1 == num2 :
+  print(f"Both numbers are equal ")
+elif num1 > num2 :
+  print(f"{num1} is greater than {num2}")
+else:
+  print(f"{num2} is greater than {num1}")
+
+#Step 3: Check if any number is zero
+
+if num1 == 0 or num2 ==0:
+  print("\n At least one number is zero")
+else:
+  print("\n Both numbers are non-zero")
